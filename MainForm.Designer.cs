ddfc08e1adb07af801525a81e7cac5cc90472fe2
ITwisterMax <solution.m@mail.ru>
2022-03-06 01:39:45 +0300

    Add lab work 3

---
+++ MainForm.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Laba3
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.tbFalseAlarmProbability = new System.Windows.Forms.TextBox();
+            this.tbSecondProbability = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbProbabilityOfMissingErrorDetection = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbFirstProbability = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnClassify = new System.Windows.Forms.Button();
+            this.tbProbabilityOfTotalClassificationError = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gbSettings = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.gbSettings.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pictureBox.Location = new System.Drawing.Point(14, 15);
+            this.pictureBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(598, 420);
+            this.pictureBox.TabIndex = 3;
+            this.pictureBox.TabStop = false;
+            // 
+            // tbFalseAlarmProbability
+            // 
+            this.tbFalseAlarmProbability.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.tbFalseAlarmProbability.Enabled = false;
+            this.tbFalseAlarmProbability.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.tbFalseAlarmProbability.Location = new System.Drawing.Point(10, 216);
+            this.tbFalseAlarmProbability.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbFalseAlarmProbability.Name = "tbFalseAlarmProbability";
+            this.tbFalseAlarmProbability.Size = new System.Drawing.Size(234, 34);
+            this.tbFalseAlarmProbability.TabIndex = 4;
+            // 
+            // tbSecondProbability
+            // 
+            this.tbSecondProbability.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.tbSecondProbability.Font = new System.Drawing.Font("Arial Narrow", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbSecondProbability.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.tbSecondProbability.Location = new System.Drawing.Point(82, 84);
+            this.tbSecondProbability.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbSecondProbability.Name = "tbSecondProbability";
+            this.tbSecondProbability.Size = new System.Drawing.Size(162, 34);
+            this.tbSecondProbability.TabIndex = 2;
+            this.tbSecondProbability.Text = "0.6";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 183);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(128, 29);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "False alarm:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(8, 87);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 29);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "P2(x):";
+            // 
+            // tbProbabilityOfMissingErrorDetection
+            // 
+            this.tbProbabilityOfMissingErrorDetection.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.tbProbabilityOfMissingErrorDetection.Enabled = false;
+            this.tbProbabilityOfMissingErrorDetection.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.tbProbabilityOfMissingErrorDetection.Location = new System.Drawing.Point(10, 287);
+            this.tbProbabilityOfMissingErrorDetection.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbProbabilityOfMissingErrorDetection.Name = "tbProbabilityOfMissingErrorDetection";
+            this.tbProbabilityOfMissingErrorDetection.Size = new System.Drawing.Size(234, 34);
+            this.tbProbabilityOfMissingErrorDetection.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 254);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(237, 29);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Missing error detection:";
+            // 
+            // tbFirstProbability
+            // 
+            this.tbFirstProbability.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.tbFirstProbability.Font = new System.Drawing.Font("Arial Narrow", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbFirstProbability.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.tbFirstProbability.Location = new System.Drawing.Point(81, 42);
+            this.tbFirstProbability.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbFirstProbability.Name = "tbFirstProbability";
+            this.tbFirstProbability.Size = new System.Drawing.Size(163, 34);
+            this.tbFirstProbability.TabIndex = 1;
+            this.tbFirstProbability.Text = "0.4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(5, 325);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(248, 29);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Total classification error:";
+            // 
+            // btnClassify
+            // 
+            this.btnClassify.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnClassify.FlatAppearance.BorderColor = System.Drawing.Color.LightSlateGray;
+            this.btnClassify.Location = new System.Drawing.Point(10, 126);
+            this.btnClassify.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnClassify.Name = "btnClassify";
+            this.btnClassify.Size = new System.Drawing.Size(234, 38);
+            this.btnClassify.TabIndex = 0;
+            this.btnClassify.Text = "Classify";
+            this.btnClassify.UseVisualStyleBackColor = false;
+            this.btnClassify.Click += new System.EventHandler(this.btnClassify_Click);
+            // 
+            // tbProbabilityOfTotalClassificationError
+            // 
+            this.tbProbabilityOfTotalClassificationError.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.tbProbabilityOfTotalClassificationError.Enabled = false;
+            this.tbProbabilityOfTotalClassificationError.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.tbProbabilityOfTotalClassificationError.Location = new System.Drawing.Point(9, 358);
+            this.tbProbabilityOfTotalClassificationError.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbProbabilityOfTotalClassificationError.Name = "tbProbabilityOfTotalClassificationError";
+            this.tbProbabilityOfTotalClassificationError.Size = new System.Drawing.Size(235, 34);
+            this.tbProbabilityOfTotalClassificationError.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label1.Location = new System.Drawing.Point(8, 45);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 29);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "P1(x):";
+            // 
+            // gbSettings
+            // 
+            this.gbSettings.Controls.Add(this.label1);
+            this.gbSettings.Controls.Add(this.tbProbabilityOfTotalClassificationError);
+            this.gbSettings.Controls.Add(this.btnClassify);
+            this.gbSettings.Controls.Add(this.label5);
+            this.gbSettings.Controls.Add(this.tbFirstProbability);
+            this.gbSettings.Controls.Add(this.label4);
+            this.gbSettings.Controls.Add(this.tbProbabilityOfMissingErrorDetection);
+            this.gbSettings.Controls.Add(this.label2);
+            this.gbSettings.Controls.Add(this.label3);
+            this.gbSettings.Controls.Add(this.tbSecondProbability);
+            this.gbSettings.Controls.Add(this.tbFalseAlarmProbability);
+            this.gbSettings.Font = new System.Drawing.Font("Arial Narrow", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.gbSettings.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.gbSettings.Location = new System.Drawing.Point(620, 15);
+            this.gbSettings.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gbSettings.Name = "gbSettings";
+            this.gbSettings.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gbSettings.Size = new System.Drawing.Size(258, 420);
+            this.gbSettings.TabIndex = 12;
+            this.gbSettings.TabStop = false;
+            this.gbSettings.Text = "Settings";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SlateBlue;
+            this.ClientSize = new System.Drawing.Size(891, 448);
+            this.Controls.Add(this.gbSettings);
+            this.Controls.Add(this.pictureBox);
+            this.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "MainForm";
+            this.Text = "Probabilistic Approach";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.gbSettings.ResumeLayout(false);
+            this.gbSettings.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.TextBox tbFalseAlarmProbability;
+        private System.Windows.Forms.TextBox tbSecondProbability;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbProbabilityOfMissingErrorDetection;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbFirstProbability;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnClassify;
+        private System.Windows.Forms.TextBox tbProbabilityOfTotalClassificationError;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox gbSettings;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Drawing;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace Laba3
+{
+    /// <summary>
+    ///     Main class
+    /// </summary>
+    public partial class MainForm : Form
+    {
+        /// <summary>
+        ///     Tests count
+        /// </summary>
+        private const int NumberOfTests = 10000;
+
+        /// <summary>
+        ///     Default constructor
+        /// </summary>
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        ///     Classify button click
+        /// </summary>
+        ///
+        /// <param name="sender">Sender</param>
+        /// <param name="e">Events</param>
+        private void btnClassify_Click(object sender, EventArgs e)
+        {
+            var graphicsBoard = pictureBox.CreateGraphics();
+            graphicsBoard.Clear(Color.LightGray);
+
+            var random = new Random();
+            int[] firstRandomVariables = new int[NumberOfTests];
+            int[] secondRandomVariables = new int[NumberOfTests];
+
+            try
+            {
+                // Get propabilities
+                double firstNumber = double.Parse(tbFirstProbability.Text.Replace(".", ","));
+                double secondNumber = double.Parse(tbSecondProbability.Text.Replace(".", ","));
+
+                // Check them
+                if (IsNumberCorrect(firstNumber, secondNumber))
+                {
+                    // Get random variables
+                    for (int i = 0; i < 10000; i++)
+                    {
+                        firstRandomVariables[i] = random.Next(pictureBox.Width) - 100;
+                        secondRandomVariables[i] = random.Next(pictureBox.Width) + 100;
+                    }
+
+                    // Calculate Math expectration and standart deviation
+                    double firstMathExpectation = CalculateMathExpectation(firstRandomVariables);
+                    double secondMathExpectation = CalculateMathExpectation(secondRandomVariables);
+
+                    double firstStandardDeviation = CalculateStandardDeviation(firstRandomVariables, firstMathExpectation);
+                    double secondStandardDeviation = CalculateStandardDeviation(secondRandomVariables, secondMathExpectation);
+
+                    // Calculate propability density
+                    double[] probabilityDensityForFirstRandomVariables = new double[NumberOfTests];
+                    double[] probabilityDensityForSecondRandomVariables = new double[NumberOfTests];
+
+                    for (int i = 0; i < NumberOfTests; i++)
+                    {
+                        probabilityDensityForFirstRandomVariables[i] = CalculateProbabilityDensity(i, firstMathExpectation, firstStandardDeviation);
+                        probabilityDensityForSecondRandomVariables[i] = CalculateProbabilityDensity(i, secondMathExpectation, secondStandardDeviation);
+                    }
+
+                    // Draw graphics
+                    for (int i = 0; i < NumberOfTests; i++)
+                    {
+                        var graphics = pictureBox.CreateGraphics();
+
+                        graphics.FillRectangle(Brushes.Aqua, i, pictureBox.Height - (int) (probabilityDensityForFirstRandomVariables[i] * firstNumber * 150000), 3, 3);
+                        graphics.FillRectangle(Brushes.Coral, i, pictureBox.Height - (int) (probabilityDensityForSecondRandomVariables[i] * secondNumber * 150000), 3, 3);
+                    }
+
+                    // Calculate propabilities
+                    tbFalseAlarmProbability.Text = Convert.ToString(CalculateProbabilitiesOfFalseAlarms(
+                        firstNumber, secondNumber, firstMathExpectation, secondMathExpectation, firstStandardDeviation, secondStandardDeviation
+                        ), CultureInfo.InvariantCulture);
+                    tbProbabilityOfMissingErrorDetection.Text = Convert.ToString(CalculateProbabilitiesOfMissingErrors(
+                        firstNumber, secondNumber, firstMathExpectation, secondMathExpectation, firstStandardDeviation, secondStandardDeviation
+                        ), CultureInfo.InvariantCulture);
+                    tbProbabilityOfTotalClassificationError.Text = Convert.ToString(
+                        Convert.ToDouble(tbFalseAlarmProbability.Text.Replace(".", ",")) +
+                        Convert.ToDouble(tbProbabilityOfMissingErrorDetection.Text.Replace(".", ",")),
+                        CultureInfo.InvariantCulture);
+                }
+                else
+                {
+                    MessageBox.Show(@"The entered values ​​do not match the conditions!", @"Error", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show(@"Please enter the correct number!", @"Error", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+            }
+        }
+
+        /// <summary>
+        ///     Check propabilities
+        /// </summary>
+        ///
+        /// <param name="firstNumber">First propability</param>
+        /// <param name="secondNumber">Second propability</param>
+        ///
+        /// <returns>bool</returns>
+        public bool IsNumberCorrect(double firstNumber, double secondNumber)
+        {
+            return firstNumber >= 0 && firstNumber <= 1 && secondNumber >= 0 && secondNumber <= 1 && Math.Abs(firstNumber + secondNumber - 1) <= 0;
+        }
+
+        /// <summary>
+        ///     Calculate math expectation
+        /// </summary>
+        ///
+        /// <param name="randomVariables">Random numbers</param>
+        ///
+        /// <returns>double</returns>
+        public double CalculateMathExpectation(int[] randomVariables)
+        {
+            int sumOfRandomVariables = 0;
+            for (int i = 0; i < NumberOfTests; i++)
+            {
+                sumOfRandomVariables += randomVariables[i];
+            }
+
+            return (double)sumOfRandomVariables / NumberOfTests;
+        }
+
+        /// <summary>
+        ///     Calculate standart deviation
+        /// </summary>
+        ///
+        /// <param name="randomVariables">Random numbers</param>
+        /// <param name="mathExpectation">Math expectation</param>
+        ///
+        /// <returns>double</returns>
+        public double CalculateStandardDeviation(int[] randomVariables, double mathExpectation)
+        {
+            double sum = 0;
+            for (int i = 0; i < NumberOfTests; i++)
+            {
+                sum += Math.Pow(randomVariables[i] - mathExpectation, 2);
+            }
+
+            return Math.Sqrt(sum / NumberOfTests);
+        }
+
+        /// <summary>
+        ///     Calculate propability density
+        /// </summary>
+        ///
+        /// <param name="number">Number</param>
+        /// <param name="mathExpectation">Math expectation</param>
+        /// <param name="standardDeviation">Standart deviation</param>
+        ///
+        /// <returns>double</returns>
+        public double CalculateProbabilityDensity(double number, double mathExpectation, double standardDeviation)
+        {
+            double numerator = Math.Exp(-0.5 * Math.Pow((number - mathExpectation) / standardDeviation, 2));
+            double denominator = standardDeviation * Math.Sqrt(2 * Math.PI);
+
+            return numerator / denominator;
+        }
+
+        /// <summary>
+        ///     Calculate probabilities of false alarms
+        /// </summary>
+        ///
+        /// <param name="firstProbability">First propability</param>
+        /// <param name="secondProbability">Second propability</param>
+        /// <param name="firstMathExpectation">First math expectation</param>
+        /// <param name="secondMathExpectation">Second math expectation</param>
+        /// <param name="firstStandardDeviation">First standart deviation</param>
+        /// <param name="secondStandardDeviation">Second standart deviation</param>
+        ///
+        /// <returns>double</returns>
+        public double CalculateProbabilitiesOfFalseAlarms(
+            double firstProbability,
+            double secondProbability,
+            double firstMathExpectation,
+            double secondMathExpectation,
+            double firstStandardDeviation,
+            double secondStandardDeviation
+        ) {
+            const double eps = 0.001;
+
+            double x = -100;
+            double p1 = 1, p2 = 0;
+            double probabilitiesOfFalseAlarms = 0;
+
+            if (secondProbability != 0)
+            {
+                while (p2 < p1)
+                {
+                    p1 = firstProbability * CalculateProbabilityDensity(x, firstMathExpectation, firstStandardDeviation);
+                    p2 = secondProbability * CalculateProbabilityDensity(x, secondMathExpectation, secondStandardDeviation);
+                    probabilitiesOfFalseAlarms += p2 * eps;
+                    x += eps;
+                }
+            }
+
+            if (firstProbability == 0)
+            {
+                probabilitiesOfFalseAlarms = 1;
+            }
+            else if (secondProbability == 0)
+            {
+                probabilitiesOfFalseAlarms = 0;
+            }
+            else
+            {
+                probabilitiesOfFalseAlarms /= firstProbability;
+            }
+
+            return probabilitiesOfFalseAlarms;
+        }
+
+        /// <summary>
+        ///     Calculate probabilities of missing errors
+        /// </summary>
+        ///
+        /// <param name="firstProbability">First propability</param>
+        /// <param name="secondProbability">Second propability</param>
+        /// <param name="firstMathExpectation">First math expectation</param>
+        /// <param name="secondMathExpectation">Second math expectation</param>
+        /// <param name="firstStandardDeviation">First standart deviation</param>
+        /// <param name="secondStandardDeviation">Second standart deviation</param>
+        ///
+        /// <returns>double</returns>
+        public double CalculateProbabilitiesOfMissingErrors(
+            double firstProbability,
+            double secondProbability,
+            double firstMathExpectation,
+            double secondMathExpectation,
+            double firstStandardDeviation,
+            double secondStandardDeviation
+        ) {
+            const double eps = 0.001;
+
+            double x = -100;
+            double p1 = 1, p2 = 0;
+            double probabilitiesOfMissingErrors = 0;
+
+            if (secondProbability != 0)
+            {
+                while (p2 < p1)
+                {
+                    p1 = firstProbability * CalculateProbabilityDensity(x, firstMathExpectation, firstStandardDeviation);
+                    p2 = secondProbability * CalculateProbabilityDensity(x, secondMathExpectation, secondStandardDeviation);
+                    x += eps;
+                }
+            }
+
+            double tmp = x;
+
+            while (x < pictureBox.Width + 100)
+            {
+                p1 = CalculateProbabilityDensity(x, firstMathExpectation, firstStandardDeviation);
+                CalculateProbabilityDensity(x, secondMathExpectation, secondStandardDeviation);
+                probabilitiesOfMissingErrors += p1 * firstProbability * eps;
+                x += eps;
+            }
+
+            if (firstProbability != 0 && secondProbability != 0)
+            {
+                var graph = pictureBox.CreateGraphics();
+                graph.DrawLine(new Pen(Brushes.Black, 2), new Point((int) tmp, 0), new Point((int) tmp, pictureBox.Height));
+            }
+
+            if (firstProbability == 0)
+            {
+                probabilitiesOfMissingErrors = 0;
+            } 
+            else if (secondProbability == 0)
+            {
+                probabilitiesOfMissingErrors = 0;
+            }
+            else
+            {
+                probabilitiesOfMissingErrors /= firstProbability;
+            }
+
+            return probabilitiesOfMissingErrors;
+        }
+    }
+}
